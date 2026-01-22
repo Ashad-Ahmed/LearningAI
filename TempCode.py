1afe7d6af4d3e35c24870dd2dfc27b9f16f6517c
Ashad Ahmed <72685188+Ashad-Ahmed@users.noreply.github.com>
2026-01-22 21:26:46 +0530

    Enhance HTTP response and listening message
    Updated the server response to include headers and improved the listening message.

--- TempCode.py
+++ TempCode.py
@@ -1,13 +1,24 @@
 import socket
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server.bind(("0.0.0.0", 8080))
 server.listen(5)
 
-print("Listening on 8080")
+print("Listening on http://localhost:8080")
 
 while True:
     conn, addr = server.accept()
     print("Connection from", addr)
-    conn.sendall(b"HTTP/1.1 200 OK\r\nContent-Length: 5\r\n\r\nHello")
+
+    response = (
+        "HTTP/1.1 200 OK\r\n"
+        "Content-Type: text/plain; charset=utf-8\r\n"
+        "Content-Length: 5\r\n"
+        "Connection: close\r\n"
+        "\r\n"
+        "Hello"
+    )
+
+    conn.sendall(response.encode())
     conn.close()
